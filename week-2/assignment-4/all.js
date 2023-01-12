aceeff67d85fe315865be95fde00fa0fe8f9cd18
arakang <jennifer881030@gmail.com>
2023-01-12 23:46:08 +0800

    Request 3: Click to Show More Content Boxes

--- week-2/assignment-4/all.js
+++ week-2/assignment-4/all.js
@@ -1,6 +1,12 @@
 const coverText = document.querySelector('.cover')
+const hiddenWrap = document.querySelector('.wrap-hidden')
+const cta = document.querySelector('.cta')
 
 coverText.addEventListener('click', e => {
     e.target.textContent = 'Have a Good Time!'
+})
+
+cta.addEventListener('click', e => {
+    hiddenWrap.style.display = "flex"
 })
 
