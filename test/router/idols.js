c1da4295c624bb10b4fd74845ca02fe5adab59e1
arakang <jennifer881030@gmail.com>
2023-01-17 22:39:02 +0800

    Express router practice

--- test/router/idols.js
+++ test/router/idols.js
@@ -3,10 +3,13 @@ const express = require("express")
 const router = express.Router() //產生router物件 存入變數
 
 // 一樣是路由設定 / end-point設定 / API設計
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.send("我是 /idols 的根路徑")
 })
 
-app.get("/page", (req, res) => {
+router.get("/page", (req, res) => {
     res.json({message: "我是 /idols/page 的路徑！"}) //.json就是會拋一個 JSON 的資料
 })
+
+// [module][1] 將 router 輸出，等別人 require 引入使用
+module.exports = router
--- test/server.js
+++ test/server.js
@@ -2,11 +2,19 @@ const express = require("express")
 const app = express()
 const portNum = 3000
 
+// [module][2] 引入/router/books.js 程式
+const idolsRouter = require("./router/idols.js") //. => 當前目錄
+/*
+idolrouter 就是 router 資料夾下的 idol.js 輸出的那個 router 物件
+*/ 
 
 // 路由設定 / end-point設定 / API設計
 app.get("/", (req, res) => {
     res.send("Hi, 我是 node.js server")
 })
+
+//[module][3] 將 idols 的 request，，導入到 idolsRouter 處理
+app.use("/idols", idolsRouter)
 
 app.listen(portNum, () => {
     console.log(`Server is running at localhost: ${portNum}`)
