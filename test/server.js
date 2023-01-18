5c93fe0684ec07192d36458ec265edd7cbe4798e
arakang <jennifer881030@gmail.com>
2023-01-18 22:05:32 +0800

    Using Pug in My Express App

--- test/server.js
+++ test/server.js
@@ -2,28 +2,29 @@ const express = require("express")
 const app = express()
 const portNum = 3000
 
-// [module][2] 引入/router/books.js 程式
-const idolsRouter = require("./router/idols.js") //. => 當前目錄
-const aboutRouter = require("./router/about") // .js 可以忽略不寫
-
-/*
-idolrouter 就是 router 資料夾下的 idol.js 輸出的那個 router 物件
-*/ 
-
-// 路由設定 / end-point設定 / API設計
-app.get("/", (req, res) => {
-    res.send("Hi, 我是 node.js server")
-})
+/***引入/router/about.js 程式***/
+const aboutRouter = require("./router/about")  // 當前目錄的相對路徑，.js 可以忽略不寫
 
 
+/***告訴 Express 要使用的樣版引擎是 Pug***/
+app.set("view engine", "pug")
+
+/***assignment1***/
+// app.get("/", (req, res) => {
+//     res.send("<h1>Hello, Server!</h1>")
+// })
+/***assignment2 - getData***/
 app.get("/getData", (req, res) => {
     let num = Number(req.query.number)
     res.send(`${(1+num)*num/2}`)
 })
 
-//[module][3] 將 /idols 的 request，導入到 idolsRouter 處理
-//沒有這一步將無法成功運作
-app.use("/idols", idolsRouter)
+app.get("/", (req, res) => {
+    res.render("index") //不需要加上.pug ，因為已經設定了 view engine 是 pug
+})
+
+
+/***將 /about 的 request，導入到 aboutRouter 處理***/
 app.use("/about", aboutRouter)
 
 app.listen(portNum, () => {
