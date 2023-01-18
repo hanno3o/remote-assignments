2e499295b875fdfd59e859009d7a026d509ab206
arakang <jennifer881030@gmail.com>
2023-01-18 22:41:56 +0800

    Check if there have a hint

--- test/server.js
+++ test/server.js
@@ -21,7 +21,7 @@ app.get("/getData", (req, res) => {
 
 /***cards***/
 app.get("/card", (req, res) => {
-    res.render("card", {prompt: "Who is buried in Grant's tomb?"})
+    res.render("card", {prompt: "Who is buried in Grant's tomb?", hint:"Think about whose tomb it is?"})
 })
 
 app.get("/", (req, res) => {
