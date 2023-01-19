a434b6ae8dce22a201bfe2ff4ac69ac071a95125
arakang <jennifer881030@gmail.com>
2023-01-19 16:50:40 +0800

    Have been stuck on assignment2 for 3 hour

--- week-3/backend-project/app.js
+++ week-3/backend-project/app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello, My Server!<h1>')
 })
 
+app.get("/getData", (req, res) => {
+  if (!req.query) {
+    res.send('Lack of Parameter')
+  } else {
+    let num = Number(req.query.number)
+    res.send(`${(1+num)*num/2}`)
+  }
+}) 
+// It doesn't work...:(
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
