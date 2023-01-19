1cb013d1241450dac5b45519a1a0fa73828f849a
arakang <jennifer881030@gmail.com>
2023-01-19 22:18:17 +0800

    Serve a static HTML file named sum.html

--- week-3/backend-project/app.js
+++ week-3/backend-project/app.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000;
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello, My Server!<h1>')
 })
 
-app.get("/getData", (req, res) => {
-  if (!req.query) {
-    res.send('Lack of Parameter')
-  } else {
-    let num = Number(req.query.number)
-    res.send(`${(1+num)*num/2}`)
-  }
-}) 
-// It doesn't work...:(
+app.use ((req, res, next) => {
+  res.status(404)
+  res.send('<h1>404 Page Not found... :(</h1>')
+})
 
+app.use ((err, req, res, next) => {
+  res.status(500)
+  res.send('<h1>Internal Server Error</h1>')
+})
+
+// port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
