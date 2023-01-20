75b26d535adef06f6f4ac37ea70de528e5746ed8
arakang <jennifer881030@gmail.com>
2023-01-20 22:39:00 +0800

    Edit 'serve static files' code's place

--- week-3/backend-project/app.js
+++ week-3/backend-project/app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000;
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello, My Server!<h1>')
 })
@@ -20,6 +18,8 @@ app.get('/getData', (req, res) => {
   }
 })
 
+app.use(express.static('public'))
+
 app.use ((req, res, next) => {
   res.status(404)
   res.send('<h1>404 Page Not found... :(</h1>')
@@ -29,6 +29,7 @@ app.use ((err, req, res, next) => {
   res.status(500)
   res.send('<h1>Internal Server Error</h1>')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
