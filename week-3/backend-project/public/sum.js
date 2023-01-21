65c4de350658c71274b1c6e06aec60e2301d87de
arakang <jennifer881030@gmail.com>
2023-01-21 09:50:31 +0800

    Make the UI can get a result from the server

--- week-3/backend-project/public/sum.js
+++
@@ -1,6 +0,0 @@
-fetch('http://localhost:3000/getData?number=10')
-    .then(res => res.json())
-    .then(data => console.log(data)) 
-    .catch(error => {
-        console.log(`Error: ${error}`);
-    })
