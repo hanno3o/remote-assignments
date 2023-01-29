b3c071dfebab0a3de233e5459e4b89eb4d4a3600
arakang <jennifer881030@gmail.com>
2023-01-29 18:19:13 +0800

    Embed JavaScript Expressions in JSX

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,22 +1,18 @@
-const title = React.createElement(
-    'h1',
-    {id: 'main-title', title: 'This is a title'},
-    'My firsr React element!'
+const title = 'My first React element!'
+const desc = 'I just learned how to create a React node and render it into the DOM.'
+const myTitleId = 'main-title'
+const name = 'hanno3o'
+const header = (
+    <header>
+        <h1 id= { myTitleId }>{ name }'s first React element!</h1>
+        <p className="main-desc">{ desc }</p> 
+        {/*像是在 HTML 中的 class, label 的 for，由於在 JS 中是保留字(preserve word)，所以需要額外改寫成 className 與 htmlFor*/}
+        {/*這個是 JSX 的 comment 寫法*/}
+        <input value= { 2*30*4 } />
+    </header>
 )
+ 
 
-
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-)
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
-)
 
 ReactDOM.render(
     header,
