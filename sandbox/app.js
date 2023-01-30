643862e8af9e85c14ca61f6ce035a387dd38d7d1
arakang <jennifer881030@gmail.com>
2023-01-30 10:08:03 +0800

    Create the Player Component

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,20 +1,25 @@
-const title = 'My first React element!'
-const desc = 'I just learned how to create a React node and render it into the DOM.'
-const myTitleId = 'main-title'
-const name = 'hanno3o'
-const header = (
-    <header>
-        <h1 id= { myTitleId }>{ name }'s first React element!</h1>
-        <p className="main-desc">{ desc }</p> 
-        {/*像是在 HTML 中的 class, label 的 for，由於在 JS 中是保留字(preserve word)，所以需要額外改寫成 className 與 htmlFor*/}
-        {/*這個是 JSX 的 comment 寫法*/}
-        <input value= { 2*30*4 } />
-    </header>
+const Header = () =>  (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">player: 1</span>
+        </header>
+        
 )
- 
 
+const Player = () => (
+    <div className="player">
+        <span className="player-name">
+            hanno3o
+        </span>
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    </div>
+)
 
 ReactDOM.render(
-    header,
+    <Player />,
     document.getElementById('root')
 )
