e0a37fa64c64c73c29add433d9fdfdaedbc124e5
arakang <jennifer881030@gmail.com>
2023-01-30 11:02:11 +0800

    Composing Components

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,25 +1,41 @@
-const Header = () =>  (
+const Header = () =>  {
+    return (
         <header>
             <h1>Scoreboard</h1>
             <span className="stats">player: 1</span>
         </header>
-        
-)
+    )
+}
 
-const Player = () => (
-    <div className="player">
-        <span className="player-name">
-            hanno3o
-        </span>
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
-            <button className="counter-action increment"> + </button>
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                hanno3o
+            </span>
+            <Counter />
         </div>
+    )
+}
+
+const Counter = () => (
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
     </div>
 )
 
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+            <Player />
+        </div>
+    )
+} 
+
 ReactDOM.render(
-    <Player />,
+    <App />,
     document.getElementById('root')
-)
+) 
