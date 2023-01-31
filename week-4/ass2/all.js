5b028f37af16f097ec4af9c48340ea56f786be21
arakang <jennifer881030@gmail.com>
2023-01-31 12:16:22 +0800

    Create week4 assignments' folders

---
+++ week-4/ass2/all.js
@@ -0,0 +1,13 @@
+function ajax(url) {
+    // your code here
+}
+
+function render(data) {
+    // your code here
+}
+
+const url = "https://appworks-school.github.io/Remote-Assignment-Data/products&quot"
+    ajax(url).then((data) => {
+    render(data)
+    })
+// you should get product information in JSON format and render data in the page
