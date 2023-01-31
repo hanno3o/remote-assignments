841febc941ff2549bb223b41620f4e795d06e1c4
arakang <jennifer881030@gmail.com>
2023-01-31 17:13:52 +0800

    Assignment 2: HTML DOM and AJAX

--- week-4/ass2/all.js
+++ week-4/ass2/all.js
@@ -1,13 +1,24 @@
+const url = "https://appworks-school.github.io/Remote-Assignment-Data/products"
+const result = document.querySelector('.result')
+
 function ajax(url) {
-    // your code here
+    return fetch(url)
+    .then(res => res.json())
 }
 
 function render(data) {
-    // your code here
+    data.forEach(function(item, index, array){
+        result.innerHTML += `
+        <h2>${data[index].name}</h2>
+        <hr>
+        <p style="font-weight: 100;">$${data[index].price}</p>
+        <p>${data[index].description}</p>
+        <br>
+        `
+    }) 
 }
 
-const url = "https://appworks-school.github.io/Remote-Assignment-Data/products&quot"
-    ajax(url).then((data) => {
+ajax(url).then(data => {
     render(data)
-    })
+})
 // you should get product information in JSON format and render data in the page
