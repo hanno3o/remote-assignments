91316401e282758562a4ec04aea85421dfd45a37
arakang <jennifer881030@gmail.com>
2023-02-01 22:29:15 +0800

    Build static page layout

---
+++ week-4/react-webpage/src/pages/Home/components/Cover.js
@@ -0,0 +1,7 @@
+const Cover = () => {
+    return <div className="cover">
+    ❝ 내가 아닌 모습으로 사랑받을 바에는 나의 모습으로 미움 받겠다 ❞
+    </div>
+}
+
+export default Cover
---
+++ week-4/react-webpage/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Nav from './components/Nav'
+import Cover from './components/Cover'
+import Session from './components/Session'
+import './index.css'
+
+const Home = () => {
+    return <div>
+        <Nav />
+        <Cover />
+        <Session />
+    </div>
+}
+
+export default Home
