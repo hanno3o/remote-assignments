1b01eda2f1d13b2769bcea1e65861b88a4e2741f
arakang <jennifer881030@gmail.com>
2023-02-04 08:55:20 +0800

    Rendering repeat components with map()

--- week-4/react/my-app/src/pages/Home/components/Nav.js
+++ week-4/react/my-app/src/pages/Home/components/Nav.js
@@ -2,6 +2,38 @@ import logo from "../logo.png"
 import React, { useState } from 'react'
 
 const Nav = () => {
+    const links = [
+        {
+            href: "https://zh.m.wikipedia.org/zh-tw/(G)I-DLE",
+            linkName: "(G)I-DLE",
+            id: 1
+        },
+        {
+            href: "https://www.dcard.tw/f/entertainer/p/233889837",
+            linkName: "成員介紹",
+            id: 2
+        },
+        {
+            href: "https://cafe.daum.net/cube-g-i-dle",
+            linkName: "官咖",
+            id: 3
+        },
+        {
+            href: "https://youtube.com/playlist?list=PLeLhwIKudpq9ifaxPqrsWPZ2_7gm2n85T",
+            linkName: "發行歌曲",
+            id: 4
+        }
+    ]
+    const linkItems = links.map(link => {
+        return <li>
+            <a 
+            href={link.href}
+            key={link.id}
+            >
+            {link.linkName}
+            </a>
+        </li>
+    })
     const [active, setActive] = useState("")
     return <div>  
         <nav>
@@ -18,10 +50,7 @@ const Nav = () => {
                 </div>
             </label>
             <ul className={active}>
-                <li><a href="https://zh.m.wikipedia.org/zh-tw/(G)I-DLE">(G)I-DLE</a></li>
-                <li><a href="https://www.dcard.tw/f/entertainer/p/233889837">成員介紹</a></li>
-                <li><a href="https://cafe.daum.net/cube-g-i-dle">官咖</a></li>
-                <li><a href="https://youtube.com/playlist?list=PLeLhwIKudpq9ifaxPqrsWPZ2_7gm2n85T">發行歌曲</a></li>
+                {linkItems}
             </ul>
         </nav> 
     </div>
--- week-4/react/my-app/src/pages/Home/components/Session.js
+++ week-4/react/my-app/src/pages/Home/components/Session.js
@@ -1,15 +1,73 @@
 import { useState } from "react"
 
 const Session = () => {
+    const boxes = [
+        {
+            className: "content-box box-1",
+            boxName: "Nxde",
+            id: 1
+        },
+        {
+            className: "content-box box-2",
+            boxName: "Tomboy",
+            id: 2
+        },
+        {
+            className: "content-box box-3",
+            boxName: "MY BAG",
+            id: 3
+        },
+        {
+            className: "content-box box-4",
+            boxName: "Latata",
+            id: 4
+        }
+    ]
+    const hiddenBoxes = [
+        {
+            className: "content-box box-5",
+            boxName: "Hann",
+            id: 1
+        },
+        {
+            className: "content-box box-6",
+            boxName: "Dumdi Dumdi",
+            id: 2
+        },
+        {
+            className: "content-box box-7",
+            boxName: "Villan Dies",
+            id: 3
+        },
+        {
+            className: "content-box box-8",
+            boxName: "Oh My God",
+            id: 4
+        }
+    ]
+        
+    const boxItems = boxes.map(box => {
+        return <div 
+        className={box.className}
+        key={box.id}
+        >
+            {box.boxName}
+        </div>
+    })
+    const hiddenBoxItems = hiddenBoxes.map(box => {
+        return <div 
+        className={box.className}
+        key={box.id}
+        >
+            {box.boxName}
+        </div>
+    })
     const [hiddenWrap, showHiddenWrap] = useState("wrap-hidden")
     return <div>
         <section>
             <h3><i className="fa-solid fa-fire"></i> Hit Songs</h3>
             <div className="wrap">
-                <div className="content-box box-1">Nxde</div>
-                <div className="content-box box-2">Tomboy</div>
-                <div className="content-box box-3">MY BAG</div>
-                <div className="content-box box-4">Latata</div>
+                {boxItems}
             </div>
             <a href="#" 
                onClick={() => showHiddenWrap("wrap")}
@@ -17,10 +75,7 @@ const Session = () => {
                看更多歌曲
             </a>
             <div className={hiddenWrap}>
-                <div className="content-box box-5">Hann</div>
-                <div className="content-box box-6">Dumdi Dumdi</div>
-                <div className="content-box box-7">Villain Dies</div>
-                <div className="content-box box-8">Oh My God</div>
+                {hiddenBoxItems}
             </div>
         </section>
     </div>
